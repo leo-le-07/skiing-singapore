c666480bca6e4eac00bd25ce64f6a15e0b4ead5e
Leo Le <coder.leo.le@gmail.com>
2016-09-15 14:21:11 +0700

    refactor code

--- skiing-solution.rb
+++ skiing-solution.rb
@@ -1,12 +1,13 @@
 class Elevation
-  attr_accessor :position, :parent, :value, :weight, :root
+  attr_accessor :coor_r, :coor_c, :parent, :value, :weight, :root, :length_to_root
 
-  def initialize(row, column, value)
-    @position = Array.new
-    @position.push(row).push(column)
+  def initialize(coor_r, coor_c, value)
+    @coor_r = coor_r
+    @coor_c = coor_c
     @value = value
     @parent = nil
     @root = nil
+    @length_to_root = 0
     @weight = 1
   end
 end
@@ -15,8 +16,7 @@ class Skii
   def initialize
     @input_arr = Array.new
     @sorted_arr = Array.new
-    @visited_elevations = Hash.new
-    @key_weightest = nil
+    @weightest_elevation = nil
   end
 
   def load_input(file_name)
@@ -32,29 +32,26 @@ class Skii
   end
 
   def sort_array
-    start = Time.now
     (0..@arr_row - 1).each do |r|
       (0..@arr_column - 1).each do |c|
         @sorted_arr.push([r, c])
       end
     end
-
+    # sort index array by decreasing value of input array
     @sorted_arr.sort_by! {|e| -@input_arr[e[0]][e[1]]}
-    finish = Time.now
-    puts "Sorting time: #{finish - start}"
-  end
-
-  def gen_key(i, j)
-    "#{i}-#{j}"
   end
 
   def find_path
     start = Time.now
+    dr = [0, 0, -1, 1]
+    dc = [-1, 1, 0, 0]
+    track_arr = Array.new(@arr_row){Array.new(@arr_column)}
+
     @sorted_arr.each do |coord|
       r = coord[0]
       c = coord[1]
       elevation = Elevation.new(r, c, @input_arr[r][c])
-      weightest_elevation = weightest_elevation_around(elevation)
+      weightest_elevation = weightest_elevation_around(elevation, track_arr, dr, dc)
       elevation.parent = weightest_elevation
       unless weightest_elevation.nil?
         elevation.weight = weightest_elevation.weight + 1
@@ -62,45 +59,41 @@ class Skii
       else
         elevation.root = elevation
       end
-      @visited_elevations[gen_key(elevation.position[0], elevation.position[1])] = elevation
-      
-      update_key_weightest(elevation)
+      elevation.length_to_root = elevation.root.value - elevation.value
+      track_arr[r][c] = elevation
+      update_key_weightest(elevation, track_arr)
     end
     finish = Time.now
     puts "Finding time: #{finish - start}"
   end
 
-  def update_key_weightest(elevation)
-    current_key = gen_key(elevation.position[0], elevation.position[1])
-    if ((@key_weightest.nil?) ||
-        (!@key_weightest.nil? && @visited_elevations[current_key].weight > @visited_elevations[@key_weightest].weight) ||
-        (!@key_weightest.nil? && @visited_elevations[current_key].weight == @visited_elevations[@key_weightest].weight &&
-           (@visited_elevations[current_key].root.value - @visited_elevations[current_key].value) > (@visited_elevations[@key_weightest].root.value - @visited_elevations[@key_weightest].value)))
-      @key_weightest = current_key
+  def update_key_weightest(elevation, track_arr)
+    if (@weightest_elevation.nil? ||
+        elevation.weight > @weightest_elevation.weight ||
+        (elevation.weight == @weightest_elevation.weight &&
+          elevation.length_to_root > @weightest_elevation.length_to_root))
+      @weightest_elevation = elevation
     end
   end
 
-  def weightest_elevation_around(elevation)
-    i = elevation.position[0]
-    j = elevation.position[1]
+  def weightest_elevation_around(elevation, track_arr, dr, dc)
     weightest_elevation = nil
+    (0..3).each do |num|
+      adj_r = elevation.coor_r + dr[num]
+      adj_c = elevation.coor_c + dc[num]
 
-    weightest_elevation = get_weightest_elevation(weightest_elevation, elevation, [i, j-1])
-    weightest_elevation = get_weightest_elevation(weightest_elevation, elevation, [i+1, j])
-    weightest_elevation = get_weightest_elevation(weightest_elevation, elevation, [i, j+1])
-    weightest_elevation = get_weightest_elevation(weightest_elevation, elevation, [i-1, j])
+      # next if coordinates out of map size
+      if (adj_r < 0 || adj_c < 0 || adj_r >= @arr_row || adj_c >= @arr_column)
+        next
+      end
 
-    weightest_elevation
-  end
-
-  def get_weightest_elevation(weightest_elevation, elevation, position)
-    if (@visited_elevations.has_key?(gen_key(position[0], position[1])) && @visited_elevations[gen_key(position[0], position[1])].value > elevation.value)
-      compared_elevation = @visited_elevations[gen_key(position[0], position[1])]
-      
-      if (weightest_elevation.nil?) ||
-        (!weightest_elevation.nil? && weightest_elevation.weight < compared_elevation.weight) ||
-        (!weightest_elevation.nil? && weightest_elevation.weight == compared_elevation.weight && weightest_elevation.value < compared_elevation.value)
-        weightest_elevation = compared_elevation
+      if (@input_arr[elevation.coor_r][elevation.coor_c] < @input_arr[adj_r][adj_c])
+        adjacent = track_arr[adj_r][adj_c]
+        if (weightest_elevation.nil?) ||
+          (weightest_elevation.weight < adjacent.weight) ||
+          (weightest_elevation.weight == adjacent.weight && weightest_elevation.value < adjacent.value)
+          weightest_elevation = adjacent
+        end
       end
     end
     weightest_elevation
@@ -109,41 +102,23 @@ class Skii
   def run
     sort_array()
     find_path()
-
     print_result()
-    print_longest_path()
-    #print_sorted_arr()
-    #print_hash_elevation()
   end
 
   private
-    def print_sorted_arr
-      @arr_sorted.each do |val|
-        puts "#{val[2]} (#{val[0]}, #{val[1]})"
-      end
-    end
-
     def print_result
-      result_str = ""
-      #puts @visited_elevations[@key_weightest].root.value
-      #puts @visited_elevations[@key_weightest].value
-      puts "#{@visited_elevations[@key_weightest].root.value.to_i - @visited_elevations[@key_weightest].value.to_i } #{@visited_elevations[@key_weightest].weight}"
-    end
-
-    def print_hash_elevation
-      @visited_elevations.each do |key, value|
-        puts "#{key} => #{value.value} (#{value.position[0]}, #{value.position[1]}) : #{value.weight}"
-      end
-    end
-
-    def print_longest_path
-      result_str = ""
-      start_elevation = @visited_elevations[@key_weightest]
+      path = ""
+      start_elevation = @weightest_elevation
+      length = 0
       while start_elevation != nil do
-        result_str.prepend("#{start_elevation.value} ")
+        length = length + 1
+        path.prepend("#{start_elevation.value} ")
         start_elevation = start_elevation.parent
       end
-      puts result_str
+
+      puts "Length: #{length}"
+      puts "Drop: #{@weightest_elevation.length_to_root}"
+      puts "Path: #{path}"
     end
 end
 
