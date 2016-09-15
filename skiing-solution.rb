62ae25a7b80130412319596aa5334a23b31f8ec8
Leo Le <coder.leo.le@gmail.com>
2016-09-15 11:06:25 +0700

    replace topology sort by decreasing sort array

--- skiing-solution.rb
+++ skiing-solution.rb
@@ -13,10 +13,9 @@ end
 
 class Skii
   def initialize
-    @arr_input = Array.new
-    @arr_sorted = Array.new
-    @hash_visited = Hash.new
-    @hash_elevations = Hash.new
+    @input_arr = Array.new
+    @sorted_arr = Array.new
+    @visited_elevations = Hash.new
     @key_weightest = nil
   end
 
@@ -27,54 +26,34 @@ class Skii
         @arr_row = arr[0]
         @arr_column = arr[1]
       else
-        @arr_input.push(arr)
+        @input_arr.push(arr)
       end
     end
   end
 
-  def sort_arr_input_by_topology
+  def sort_array
     start = Time.now
-    @arr_input.each_with_index do |x, idx_row|
-      x.each_with_index do |cell, idx_col|
-        if unvisited?(idx_row, idx_col)
-          visit(idx_row, idx_col)
-        end
+    (0..@arr_row - 1).each do |r|
+      (0..@arr_column - 1).each do |c|
+        @sorted_arr.push([r, c])
       end
     end
+
+    @sorted_arr.sort_by! {|e| -@input_arr[e[0]][e[1]]}
     finish = Time.now
-    puts "Sorting time #{finish - start}"
-  end
-
-  def visit(i, j)
-    @hash_visited[gen_key(i, j)] = 1
-    if (j > 0 && @arr_input[i][j-1] < @arr_input[i][j] && unvisited?(i, j-1))
-      visit(i, j-1)
-    end
-    if (i < @arr_row - 1 && @arr_input[i+1][j] < @arr_input[i][j] && unvisited?(i+1, j))
-      visit(i+1, j)
-    end
-    if (j < @arr_column - 1 && @arr_input[i][j+1] < @arr_input[i][j] && unvisited?(i, j+1))
-      visit(i, j+1)
-    end
-    if (i > 0 && @arr_input[i-1][j] < @arr_input[i][j] && unvisited?(i-1, j))
-      visit(i-1, j)
-    end
-    tmp = Array.new
-    tmp.push(i).push(j).push(@arr_input[i][j])
-    @arr_sorted.push(tmp)
-  end
-
-  def unvisited?(i, j)
-    !@hash_visited.has_key?(gen_key(i, j))
+    puts "Sorting time: #{finish - start}"
   end
 
   def gen_key(i, j)
     "#{i}-#{j}"
   end
 
-  def find_longest_deeper_path
-    @arr_sorted.reverse_each do |e|
-      elevation = Elevation.new(e[0], e[1], e[2])
+  def find_path
+    start = Time.now
+    @sorted_arr.each do |coord|
+      r = coord[0]
+      c = coord[1]
+      elevation = Elevation.new(r, c, @input_arr[r][c])
       weightest_elevation = weightest_elevation_around(elevation)
       elevation.parent = weightest_elevation
       unless weightest_elevation.nil?
@@ -83,18 +62,20 @@ class Skii
       else
         elevation.root = elevation
       end
-      @hash_elevations[gen_key(elevation.position[0], elevation.position[1])] = elevation
+      @visited_elevations[gen_key(elevation.position[0], elevation.position[1])] = elevation
       
       update_key_weightest(elevation)
     end
+    finish = Time.now
+    puts "Finding time: #{finish - start}"
   end
 
   def update_key_weightest(elevation)
     current_key = gen_key(elevation.position[0], elevation.position[1])
     if ((@key_weightest.nil?) ||
-        (!@key_weightest.nil? && @hash_elevations[current_key].weight > @hash_elevations[@key_weightest].weight) ||
-        (!@key_weightest.nil? && @hash_elevations[current_key].weight == @hash_elevations[@key_weightest].weight &&
-           (@hash_elevations[current_key].root.value - @hash_elevations[current_key].value) > (@hash_elevations[@key_weightest].root.value - @hash_elevations[@key_weightest].value)))
+        (!@key_weightest.nil? && @visited_elevations[current_key].weight > @visited_elevations[@key_weightest].weight) ||
+        (!@key_weightest.nil? && @visited_elevations[current_key].weight == @visited_elevations[@key_weightest].weight &&
+           (@visited_elevations[current_key].root.value - @visited_elevations[current_key].value) > (@visited_elevations[@key_weightest].root.value - @visited_elevations[@key_weightest].value)))
       @key_weightest = current_key
     end
   end
@@ -113,8 +94,8 @@ class Skii
   end
 
   def get_weightest_elevation(weightest_elevation, elevation, position)
-    if (@hash_elevations.has_key?(gen_key(position[0], position[1])) && @hash_elevations[gen_key(position[0], position[1])].value > elevation.value)
-      compared_elevation = @hash_elevations[gen_key(position[0], position[1])]
+    if (@visited_elevations.has_key?(gen_key(position[0], position[1])) && @visited_elevations[gen_key(position[0], position[1])].value > elevation.value)
+      compared_elevation = @visited_elevations[gen_key(position[0], position[1])]
       
       if (weightest_elevation.nil?) ||
         (!weightest_elevation.nil? && weightest_elevation.weight < compared_elevation.weight) ||
@@ -126,20 +107,17 @@ class Skii
   end
 
   def run
-    start = Time.now
-    sort_arr_input_by_topology()
-    find_longest_deeper_path()
-    finish = Time.now
-    puts "Time finding: #{finish - start}"
+    sort_array()
+    find_path()
 
     print_result()
-    #print_longest_path()
-    #print_arr_topo()
+    print_longest_path()
+    #print_sorted_arr()
     #print_hash_elevation()
   end
 
   private
-    def print_arr_topo
+    def print_sorted_arr
       @arr_sorted.each do |val|
         puts "#{val[2]} (#{val[0]}, #{val[1]})"
       end
@@ -147,20 +125,20 @@ class Skii
 
     def print_result
       result_str = ""
-      #puts @hash_elevations[@key_weightest].root.value
-      #puts @hash_elevations[@key_weightest].value
-      puts "#{@hash_elevations[@key_weightest].root.value.to_i - @hash_elevations[@key_weightest].value.to_i } #{@hash_elevations[@key_weightest].weight}"
+      #puts @visited_elevations[@key_weightest].root.value
+      #puts @visited_elevations[@key_weightest].value
+      puts "#{@visited_elevations[@key_weightest].root.value.to_i - @visited_elevations[@key_weightest].value.to_i } #{@visited_elevations[@key_weightest].weight}"
     end
 
     def print_hash_elevation
-      @hash_elevations.each do |key, value|
+      @visited_elevations.each do |key, value|
         puts "#{key} => #{value.value} (#{value.position[0]}, #{value.position[1]}) : #{value.weight}"
       end
     end
 
     def print_longest_path
       result_str = ""
-      start_elevation = @hash_elevations[@key_weightest]
+      start_elevation = @visited_elevations[@key_weightest]
       while start_elevation != nil do
         result_str.prepend("#{start_elevation.value} ")
         start_elevation = start_elevation.parent
